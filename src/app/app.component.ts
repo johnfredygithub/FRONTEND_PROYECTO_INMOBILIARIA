173a942536271d6e7d3a6abc3829926cdbe5c173
john <johnrnriquezmunoz@gmail.com>
2021-12-11 19:00:11 -0500

    segundo commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/////componentes principales 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'APP';
+}
