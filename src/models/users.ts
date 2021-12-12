bce877d92a39c08c66506fda54963e215466f439
john <johnrnriquezmunoz@gmail.com>
2021-12-12 08:36:31 -0500

    consumiendo metodos /asesor desde component users

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,10 +1,14 @@
 export interface Users{
-    username:string,
-    email:any,
-    password:string,
-    tipo_user:string,
-    estado:string,
-    
+    Ase_Id: number,
+    Ase_Nombres: string,
+    Ase_Apellidos: string,
+    Ase_Cedula: string,
+    Ase_Telefono: string,
+    Ase_Direccion: string,
+    Ase_Email: string,
+    FechaInicio: string,
+    Adm_Id:number
+      
     
    
 }
